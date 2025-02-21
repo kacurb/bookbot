bffe13f93f90e34faf8981d07192442230f70cb3
Kacurb <urbani4k@gmail.com>
2025-02-21 01:12:44 +0100

    added nice report for char counter

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ def words_counter(text: str)->int:
     text = text.split()
     print(len(text))
 
+def char_counter(text: str)->dict:
+    result = {}
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'z', 'y', 'x', 'v', 'q']
+    for i in text.lower():
+        if i in alpha:
+            if i in result:
+                result[i] += 1
+            else:
+                result[i] = 1
+    for j in result:    
+        print(f"The '{j}' character was found {result[j]} times")
+    
+    
 
 
 
 def main():
     text = books_reader()
-    words_counter(text)
+    char_counter(text)
 
 
 
